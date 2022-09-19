eb36695c44c56d48795736f664d90ec60ca09c2c
Teklemariam Kflemariam Weldehawariat <teklew17@uia.no>
2022-09-19 22:02:18 +0200

    Adding MT_OnlineOrder project to git

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MT_OnlineOrder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
